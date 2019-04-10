67c11fb64a03e6c33f05f63f9110bc19a73d91ef
heo <wone2287@gmail.com>
2019-04-10 21:34:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 #!/usr/local/bin/pypy3
 # coding: utf-8
+
+'''
+
+명령어 전달 형식
+argv list value
+
+pypy3 main.py "keyword" "2019-01-01" "2019-01-03" "sim or date" 
+	       argv[1]    argv[2]      argv[3]      argv[4]
+
+'''
+
 from multiprocessing import Pool
 import webcrawling
 import naverblog
@@ -7,27 +18,27 @@ import time
 import sys
 import os
 
-'''
-명령어 전달 형식
-arguments passing
+# 프로그램 실행시간 측정
+start_time = time.time()
 
-pypy3 main.py "keyword" "2019-01-01" "2019-01-03" "sim or date" 
-	      argv[1]     argv[2]      argv[3]      argv[4]
+# 검색 키워드
+keyword = sys.argv[1]
+search_option = sys.argv[4]
+start_date = sys.argv[2].replace("-","") 
+end_date = sys.argv[3].replace("-","") 
 
-'''
+# 네이버 api 파싱 후 블로그 링크, 날짜 데이터 리스트로 리턴
+(blog_list, blog_date) = naverblog.blog_searcher(keyword, search_option) 
 
-start_time = time.time() 
-data = sys.argv[1] # 검색 할 keyword
-(blog_list, blog_date) = naverblog.blog_searcher(sys.argv[1], sys.argv[4]) # 네이버 api 파싱 후 블로그 링크, 날짜 데이터 리스트로 리턴
+# 지정한 날짜에 맞는 블로그 리스트만 리턴
+link_date = naverblog.date_search(int(start_date), int(end_date), blog_list, blog_date)
 
-start_date = sys.argv[2].replace("-","") # 2019-01-01 -> 20190101
-end_date = sys.argv[3].replace("-","") # 2019-01-03 -> 20190103
-	
-link_date = naverblog.date_search(int(start_date), int(end_date), blog_list, blog_date) # 지정한 날짜에 맞는 블로그 리스트만 리턴
+# 링크 데이터 크롤링 가능하게 변환 
+link_list = webcrawling.compile_link(link_date) 
 
-link_list = webcrawling.compile_link(link_date) # 링크 데이터 크롤링 가능하게 변환 
-pool = Pool(processes=8) # multiprocessing core 8 	
-pool.map(webcrawling.gets_link, link_list) # use multiprocessing Pool
+# 멀티프로세싱으로 크롤링 함수 실행 (process=8)
+pool = Pool(processes=8)
+pool.map(webcrawling.gets_link, link_list) 
 
 pool.terminate()
 print("<h4>--- %s seconds ---</h4>" %(time.time() - start_time))
