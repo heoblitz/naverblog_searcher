4ec403b1878a10ff35f6bebde5988b84ddaaa939
heo <wone2287@gmail.com>
2019-07-26 18:32:53 +0900

    Update search.php

--- src/search.php
+++ src/search.php
@@ -1,13 +1,12 @@
 <!DOCTYPE html>
-<!-- saved from url=(0060)http://bootstrapk.com/examples/sticky-footer-navbar/#contact -->
 <?php $msg = date("Y-m-d h:i:s"); ?>
-<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<html lang="ko">
+<head>
+    
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
     <link rel="icon" href="http://bootstrapk.com/favicon.ico">
 
     <title>Naver blog searcher</title>
